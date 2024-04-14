5d22862a89b15eb91a5992de0f6287891bd1f48d
Amazing Grace <adambguymon@gmail.com>
2024-04-14 15:45:33 -0600

    adding gitignore

---
+++ src/routes/assignments/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Calendar from "../../calendar.svelte";
+
+    
+    const assignments = [
+      { name: 'Assignment 1', dueDate: new Date(2024, 3, 10) }, // April 10, 2024
+      { name: 'Assignment 2', dueDate: new Date(2024, 3, 15) }, // April 15, 2024
+      { name: 'Assignment 3', dueDate: new Date(2024, 3, 20) }  // April 20, 2024
+      // Add more assignments as needed
+    ];
+    let name=""
+    let dueDate=""
+    function handleSubmit(){
+        console.log("submit")
+    }
+
+</script>
+<svelte:head>
+    <title>Assignments</title>
+</svelte:head>
+<h1>Assignments</h1>
+<form on:submit|preventDefault={handleSubmit}>
+    <label>
+        assignment:
+        <input type="text" bind:value={name} />
+    </label>
+    <br>
+    <label>
+        Due Date:
+        <input type="date" bind:value={dueDate} />
+      </label>
+    <br>
+    <button type="submit">Submit</button>
+</form>
+<Calendar {assignments}></Calendar>
---
+++ src/routes/notes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+
+  let title = '';
+  let content = '';
+
+  function handleSubmit() {
+    if (title.trim() === '' || content.trim() === '') {
+      alert('Please fill in both title and content');
+      return;
+    }
+
+    // You can perform further actions here, like saving the note to a database or emitting an event to notify parent components.
+    console.log('Note submitted:', { title, content });
+
+    // Reset the form fields after submission
+    title = '';
+    content = '';
+  }
+
+</script>
+<svelte:head>
+    <title>Notes</title>
+</svelte:head>
+
+
+<h1>Notes</h1>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <label>
+      Title:
+      <input type="text" bind:value={title} />
+    </label>
+    <br>
+    <label>
+      Content:
+      <textarea rows="4" bind:value={content}></textarea>
+    </label>
+    <br>
+    <button type="submit">Submit</button>
+  </form>
+  
+  
